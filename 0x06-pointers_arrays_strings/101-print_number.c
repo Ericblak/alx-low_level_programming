05e0fd64cad29f94217f842f6a1fb0a86600f05a
Your name <you@example.com>
2023-03-02 18:22:27 -0800

    nothing

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,24 @@
 #include"main.h"
-#include<stdio.h>
 
 /**
  * print_number - Prints an integer
  * @n: The integer to print
- * Return: Nothing!
+ * Return: 0
  */
 
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
-		putchar('-');
+		_putchar('-');
+		n1 = -n;
 	}
-	k /= 10;
-	if (k != 0)
-	print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
